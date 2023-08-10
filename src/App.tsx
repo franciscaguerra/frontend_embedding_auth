de274027e416b64298e4eed0672bff6b0ee86156
Francisca Guerra <franciscaguerra@Macbook-Fran-Guerra.local>
2023-08-10 02:02:55 -0400

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Login from './Login'
 import { BrowserRouter as HashRouter, Routes, Route } from 'react-router-dom';
 import Sheet from './Sheet';
 import Signup from './Signup';
+import NoPage from './NoPage';
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
         <Route path="/" element={<Login></Login>}></Route>
         <Route path="/googlesheet" element={<Sheet></Sheet>}></Route>
         <Route path="/signup" element={<Signup></Signup>}></Route>
-        <Route path="/*" element={<Login></Login>}></Route>
+        <Route path="/*" element={<NoPage></NoPage>}></Route>
       </Routes>
     </HashRouter>
   );
