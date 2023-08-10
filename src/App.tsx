50b9b444ae24f28916b11a99c2d92e8c39e656a3
Francisca Guerra <franciscaguerra@Macbook-Fran-Guerra.local>
2023-08-10 02:14:59 -0400

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import Login from './Login'
 import { BrowserRouter as HashRouter, Routes, Route } from 'react-router-dom';
 import Sheet from './Sheet';
 import Signup from './Signup';
-import NoPage from './NoPage';
 
 function App() {
 
   return (
-    <HashRouter>
+    <HashRouter basename='frontend_embedding_auth'>
       <Routes>
         <Route path="/" element={<Login></Login>}></Route>
         <Route path="/googlesheet" element={<Sheet></Sheet>}></Route>
         <Route path="/signup" element={<Signup></Signup>}></Route>
-        <Route path="/*" element={<NoPage></NoPage>}></Route>
       </Routes>
     </HashRouter>
   );
