aa90e1a860a2608b6ae7379e06d140152cc16912
Francisca Guerra <franciscaguerra@Macbook-Fran-Guerra.local>
2023-08-09 22:30:31 -0400

    bug refreshing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import Login from './Login'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter , Routes, Route } from 'react-router-dom';
 import Sheet from './Sheet';
 import Signup from './Signup';
 
 function App() {
 
   return (
-    <Router basename='/'>
-      <Routes>
+    <HashRouter>
         <Route path="/" element={<Login></Login>}></Route>
         <Route path="/googlesheet" element={<Sheet></Sheet>}></Route>
         <Route path="/signup" element={<Signup></Signup>}></Route>
-      </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
