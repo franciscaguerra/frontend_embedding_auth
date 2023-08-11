7704b79e02d7e67c9950376aa4f243c85f160bb0
Francisca Guerra <franciscaguerra@Macbook-Fran-Guerra.local>
2023-08-11 12:34:26 -0400

    push last version

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Signup from './Signup';
 function App() {
 
   return (
-    <HashRouter basename='frontend_embedding_auth'>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login></Login>}></Route>
         <Route path="/googlesheet" element={<Sheet></Sheet>}></Route>
